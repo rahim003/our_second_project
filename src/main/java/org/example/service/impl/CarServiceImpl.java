51bed20939b84a3f81af60b0edd99283990578ca
krasa <rahimkurbanov45@gmail.com>
2022-12-29 16:24:20 +0300

    first_commit_zinat1

--- src/main/java/org/example/service/impl/CarServiceImpl.java
+++ src/main/java/org/example/service/impl/CarServiceImpl.java
@@ -1,48 +1,56 @@
 package org.example.service.impl;
 
+import org.example.dao.CarDaoImpl;
 import org.example.model.Car;
 import org.example.service.CarService;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class CarServiceImpl implements CarService {
+    CarDaoImpl carDao = new CarDaoImpl();
+
+    public CarServiceImpl() throws SQLException {
+    }
+
     @Override
     public String createTable() {
-        return null;
+        return carDao.createTable();
     }
 
     @Override
     public String saveStudent(Car car) {
-        return null;
+        return carDao.saveStudent(car);
     }
 
     @Override
     public String dropTable() {
-        return null;
+
+        return carDao.dropTable();
     }
 
     @Override
     public Car findById(Long id) {
-        return null;
+        return carDao.findById(id);
     }
 
     @Override
     public Car deleteById(Long id) {
-        return null;
+        return carDao.deleteById(id);
     }
 
     @Override
     public String cleanTable() {
-        return null;
+        return carDao.cleanTable();
     }
 
     @Override
     public List<Car> findAll() {
-        return null;
+        return carDao.findAll();
     }
 
     @Override
     public Car updateStudent(Long id, Car car) {
-        return null;
+        return carDao.updateStudent(id, car);
     }
 }
